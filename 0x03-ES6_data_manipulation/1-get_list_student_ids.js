094a7999c21c5369ba257b3fa5a7c1569a1e1248
benkivuva <benkivuvak5@gmail.com>
2023-12-05 13:13:47 +0000

    fix: eslint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,7 @@
 export default function getListStudentIds(students) {
-  // Check if the input is an array
   if (!Array.isArray(students)) {
     return [];
   }
-
-  // Use map to extract ids from the array of objects
   const studentIds = students.map(student => student.id);
   return studentIds;
 }
